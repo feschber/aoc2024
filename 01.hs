e6871f96cc4dc38975ec3033e84d01ef395a5ab9
Ferdinand Schober <ferdinand.schober@fau.de>
2024-12-02 17:31:06 +0100

    simplify

--- 01.hs
+++ 01.hs
@@ -2,14 +2,8 @@ import System.IO
 import Data.List
 import Data.Maybe
 
-parseLine :: String -> [Int]
-parseLine = (map read) . words
-
-parseFile :: String -> [[Int]]
-parseFile = (map parseLine) . lines
-
 distance :: [Int] -> Maybe Int
 distance [a, b] = Just (abs (a - b))
 distance l = Nothing
 
-main = getContents >>= putStrLn . show . sum . catMaybes . (map distance) . transpose . (map sort) . transpose . parseFile
+main = getContents >>= putStrLn . show . sum . catMaybes . (map distance) . transpose . (map sort) . transpose . (map ((map read) . words)) . lines
