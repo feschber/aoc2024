c38986438728308018bbb393dbf85748362ddb49
Ferdinand Schober <ferdinand.schober@fau.de>
2024-12-07 15:01:53 +0100

    day 7 part 2

---
+++ app/day07p2.hs
@@ -0,0 +1,26 @@
+import Data.List.Split
+import Data.Maybe
+import Data.Tuple
+import Text.Read
+
+main :: IO ()
+main = getContents >>= print . sum . map fst . filter (uncurry isValid . swap) . parse
+
+asdf :: String -> Maybe [Int]
+asdf = mapM readMaybe . words
+
+parseEquation :: String -> Maybe (Int, [Int])
+parseEquation l = case splitOn ":" l of
+  [a, b] -> liftA2 (,) (readMaybe a) (asdf b)
+  _ -> Nothing
+
+parse :: String -> [(Int, [Int])]
+parse = fromJust . mapM parseEquation . lines
+
+cc :: Int -> Int -> Int
+cc a b = read (show a ++ show b)
+
+isValid :: [Int] -> Int -> Bool
+isValid [] r = r == 0
+isValid [a] r = r == a
+isValid (x : y : xs) r = isValid ((x * y) : xs) r || isValid ((x + y) : xs) r || isValid ((x `cc` y) : xs) r
