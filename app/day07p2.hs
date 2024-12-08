0d827c85e4edb09ab13267f60c95dc670a0b6328
Ferdinand Schober <ferdinand.schober@fau.de>
2024-12-08 15:04:02 +0100

    cleanup

--- app/day07p2.hs
+++ app/day07p2.hs
@@ -6,12 +6,9 @@ import Text.Read
 main :: IO ()
 main = getContents >>= print . sum . map fst . filter (uncurry isValid . swap) . parse
 
-asdf :: String -> Maybe [Int]
-asdf = mapM readMaybe . words
-
 parseEquation :: String -> Maybe (Int, [Int])
 parseEquation l = case splitOn ":" l of
-  [a, b] -> liftA2 (,) (readMaybe a) (asdf b)
+  [a, b] -> liftA2 (,) (readMaybe a) ((mapM readMaybe . words) b)
   _ -> Nothing
 
 parse :: String -> [(Int, [Int])]
